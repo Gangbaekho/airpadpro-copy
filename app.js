6a3e7f1c6498041a448e33916c9d926d2597f90e
kimjinsoo <roorooro9933@gmail.com>
2020-12-26 17:10:46 +0900

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+// END SECTION
+
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
